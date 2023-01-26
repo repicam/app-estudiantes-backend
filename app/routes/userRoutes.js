8bfd92167cdfa43b4a807ff044a03d3e0b9254a9
Ivan Repiso Camara <irepisocamara@gmail.com>
2023-01-26 08:49:36 +0100

    Modificar endpoints a ingles

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -6,7 +6,7 @@ const uploadTempFile = require('../middlewares/fileUpload')
 
 const router = express.Router()
 
-router.route('/registro').post(registroValidator, userController.registroUsuario)
+router.route('/register').post(registroValidator, userController.registroUsuario)
 router.route('/login').post(loginValidator, userController.loginUsuario)
 router.route('/renew').post(tokenValidator, userController.renovarToken)
 router.route('/user/uploadPhoto').patch(tokenValidator, uploadTempFile, userController.subirFotoUsuario)
